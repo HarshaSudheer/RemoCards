9e01d36ff7ddd8cfcea365b3a4c4314a4ba9d110
Juturu Charan Sai <charan.juturu@thoughtworks.com>
2021-11-04 18:53:18 +0530

    [Charan] First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="background">
+      <div className="menuPanel">
+        <button className="buttonStyle">
+              Host a game
+        </button>
+        <button className="buttonStyle">
+              Join a game
+        </button>
+        <button className="buttonStyle" >  
+              How to play
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
